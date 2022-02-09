c06c169b1274c46a231a165981d3145274aea26e
Taylor Masiello <masiellotay@gmail.com>
2022-02-08 23:17:47 -0800

    car can rotate (added funtionality to script)

--- DeliveryDriver2D/Assets/Driver.cs
+++ DeliveryDriver2D/Assets/Driver.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 0.1F);
     }
 }
