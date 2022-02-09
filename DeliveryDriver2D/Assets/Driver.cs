c5e633e1e07f3c3d535541c807e32674d4cea784
Taylor Masiello <masiellotay@gmail.com>
2022-02-08 23:33:02 -0800

    Car drives; added steer and move speed variables, SerializeField for both

--- DeliveryDriver2D/Assets/Driver.cs
+++ DeliveryDriver2D/Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 0.1F;
+    [SerializeField] float moveSpeed = 0.01f;
+
     void Start()
     {
         
@@ -11,8 +14,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1F);
-        transform.Translate(0, 0.01F, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
         
     }
 }
