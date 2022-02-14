98c1027813308fd801220adc3d4a2c8ce1380122
Taylor Masiello <masiellotay@gmail.com>
2022-02-14 10:12:28 -0800

    made steerAmount & moveAmount framerate independent with Time.deltaTime

--- DeliveryDriver2D/Assets/Driver.cs
+++ DeliveryDriver2D/Assets/Driver.cs
@@ -14,8 +14,8 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
