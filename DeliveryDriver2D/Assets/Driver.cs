10629af7f0f83cd4529575227bc6e200e343d411
Taylor Masiello <masiellotay@gmail.com>
2022-02-15 10:40:18 -0800

    Added boosts and slow down for bumps; filled out level; initial game mechanics completed
    next feature to implement: score counter for packages delivered, game timer; both displayed

--- DeliveryDriver2D/Assets/Driver.cs
+++ DeliveryDriver2D/Assets/Driver.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1F;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float moveSpeed = 20.0F;
+    [SerializeField] float slowSpeed = 15.0F;
+    [SerializeField] float boostSpeed = 30.0F;
 
-    void Start()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 
     void Update()
