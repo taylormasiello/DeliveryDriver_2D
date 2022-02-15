e41347f1cb88323c076781c8e43ec3087a811b71
Taylor Masiello <masiellotay@gmail.com>
2022-02-15 06:54:17 -0800

    car changes color to mimic package color to indicate package pickup

--- DeliveryDriver2D/Assets/Delivery.cs
+++ DeliveryDriver2D/Assets/Delivery.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    [SerializeField] float destroyDelay = 0.5F;
     bool hasPackage; // fasle is default bool
-    
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch");
@@ -13,18 +24,19 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-       // if (the thing we trigger is the package)
-       // then pring "package picked up" to console
-       if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package Picked Up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, destroyDelay);
         }
 
        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delilvered Package!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
